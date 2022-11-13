950647c20f1b67d2fbec11ae776206ec0c21e0f2
michael <michgboxy@gmail.com>
2022-11-13 23:49:29 +0100

    update

--- boardman.groovy
+++ boardman.groovy
@@ -1,7 +1,7 @@
 job('node_project2') {
     description("Github boardman project")
     scm{
-        git("https://github.com/michgboxy2/boardman.git", 'main')
+        git("https://github.com/michgboxy2/boardman.git", '*/main')
     }
     triggers {
         scm('* * * * *')
@@ -11,6 +11,7 @@ job('node_project2') {
     }
     steps {
         remoteShell('ubuntu@3.84.221.74:22') {
+            command('touch michy.js')
             command('git clone https://github.com/michgboxy2/boardman.git')
             command('cd boardman')
             command('sudo npm install')
